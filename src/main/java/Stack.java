51175c6605f3865f8d0221fdd4ed6031d8263b85
WSudul <wojciech.sudul@gmail.com>
2018-05-31 22:43:40 +0200

    Initial commit

---
+++ src/main/java/Stack.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Stack {
+
+    //TODO
+    List list = new LinkedList();
+
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public String top() throws IllegalStateException {
+        //dziala jak peek()
+        return null;
+    }
+
+    public String push(String e) {
+        return null;
+    }
+
+    public String pop() throws IllegalStateException {
+        return null;
+    }
+}
---
+++ src/test/java/StackTest.java
@@ -0,0 +1,32 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StackTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void isEmpty() throws Exception {
+    }
+
+    @Test
+    public void top() throws Exception {
+    }
+
+    @Test
+    public void push() throws Exception {
+    }
+
+    @Test
+    public void pop() throws Exception {
+    }
+
+}
