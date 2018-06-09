8698f7cbc11f7eabbef251d27c97097af99ab8ed
WSudul <wojciech.sudul@gmail.com>
2018-06-09 16:53:43 +0200

    fixing stack tests + formatting

--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -2,10 +2,15 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class StackTest {
     private Stack testStack;
+    private String t1 = "t1";
+    private String t2 = "t2";
+    private String t3 = "t3";
+    private String t4 = "t4";
 
     @Before
     public void setUp() throws Exception {
@@ -23,7 +28,7 @@ public class StackTest {
 
     @Test
     public void isEmptyReturnsFalseWhenHasElements() throws Exception {
-        testStack.push("element");
+        testStack.push(t1);
         assertFalse(testStack.isEmpty());
     }
 
@@ -39,24 +44,23 @@ public class StackTest {
 
     @Test
     public void topReturnsValidTopString() throws Exception {
-        String testString="test";
-        testStack.push(testString);
-        String result=testStack.pop();
-        assertEquals( testString, result);
+        testStack.push(t1);
+        testStack.push(t2);
+        String result = testStack.pop();
+        assertEquals(t2, result);
     }
 
     @Test
     public void topReturnsReferenceToPushedInstance() throws Exception {
-        String testString="test";
-        testStack.push(testString);
-        assertSame(testString, testStack.top());
+        testStack.push(t1);
+        assertSame(t1, testStack.top());
     }
 
     @Test
     public void pushAddsElements() throws Exception {
-        long initSize=testStack.size();
-        testStack.push("test");
-        assertTrue(initSize<testStack.size());
+        long expectedSize = 1;
+        testStack.push(t1);
+        assertEquals(expectedSize, testStack.size());
     }
 
     @Test
@@ -64,37 +68,59 @@ public class StackTest {
         String testString = null;
         testStack.push(testString);
         assertNull(testStack.top());
+        assertNull(testStack.pop());
     }
 
     @Test
     public void popRemovesTheTopElement() throws Exception {
-        String t1 = "t1", t2 = "te2", t3 = "t3", t4 = "t4";
         String tmp1, tmp2, tmp3, tmp4;
-        testStack.push(t1); testStack.push(t2); testStack.push(t3); testStack.push(t4);
-        tmp4=testStack.pop(); tmp3=testStack.pop(); tmp2=testStack.pop(); tmp1=testStack.pop();
-        assertEquals(t1,tmp1);
-        assertEquals(t2,tmp2);
-        assertEquals(t3,tmp3);
-        assertEquals(t4,tmp4);
+        testStack.push(t1);
+        testStack.push(t2);
+        testStack.push(t3);
+        testStack.push(t4);
+        tmp4 = testStack.pop();
+        tmp3 = testStack.pop();
+        tmp2 = testStack.pop();
+        tmp1 = testStack.pop();
+        assertEquals(t1, tmp1);
+        assertEquals(t2, tmp2);
+        assertEquals(t3, tmp3);
+        assertEquals(t4, tmp4);
     }
 
-    @Test
+    @Test(expected = IllegalStateException.class)
     public void popThrowsExceptionWhenAllElementsAreRemoved() throws Exception {
-        String t1 = "t1", t2 = "te2", t3 = "t3", t4 = "t4";
-        testStack.push(t1); testStack.push(t2); testStack.push(t3); testStack.push(t4);
-        testStack.pop(); testStack.pop(); testStack.pop(); testStack.pop();
+        testStack.push(t1);
+        testStack.push(t2);
+        testStack.push(t3);
+        testStack.push(t4);
+        testStack.pop();
+        testStack.pop();
+        testStack.pop();
+        testStack.pop();
         testStack.pop();//no elements to pop, should throw exception
 
     }
 
     @Test
     public void popRemainsInValidStateAfterThrowingException() throws Exception {
-        String t1 = "t1", t2 = "te2", t3 = "t3", t4 = "t4";
-        testStack.push(t1); testStack.push(t2); testStack.push(t3); testStack.push(t4);
-        testStack.pop(); testStack.pop(); testStack.pop(); testStack.pop();
-        testStack.pop();//no elements to pop, should throw exception
-        testStack.push("new element");
-        System.out.println(testStack.size());
+        boolean exception_thrown = false;
+        testStack.push(t1);
+        testStack.pop();
+        try {
+            testStack.pop();
+
+        } catch (IllegalStateException e) {
+            exception_thrown = true;
+        }
+        assertTrue(exception_thrown);
+        assertTrue(testStack.isEmpty());
+
+        assertSame(t2, testStack.push(t2));
+        assertFalse(testStack.isEmpty());
+        assertSame(t2, testStack.top());
+        assertSame(t2, testStack.pop());
+        assertTrue(testStack.isEmpty());
     }
 
 }
